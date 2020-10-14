a7f671642249c18ba089166054d302eb1b001089
vamseekumar7707 <vamseekumar7707@gmail.com>
2020-10-14 21:56:29 +0530

    Added: First Name

--- User_Registration.sh
+++ User_Registration.sh
@@ -1,2 +1,14 @@
 #WELCOME TO USER REGISTRATION
+#!/bin/bash -x
 
+#UC1 First Name
+
+read -p "Enter your first Name : " name
+pat="^[A-Z][a-z]{2,}$";
+
+if [[ $name =~ $pat ]]
+then
+        echo "yes";
+else
+        echo "no";
+fi
