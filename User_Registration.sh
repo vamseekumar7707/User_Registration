f9d8f98d4460c3db846e3b9e1f639a6d5608296b
vamseekumar7707 <vamseekumar7707@gmail.com>
2020-10-14 22:34:34 +0530

    Added : Password Rule 3 Numeric

--- User_Registration.sh
+++ User_Registration.sh
@@ -74,3 +74,15 @@ then
 else
         echo "Please Enter a Valid Password";
 fi
+
+#UC7 PASSWORD Rule3
+
+read -p "Enter your 8-digit password : " password
+pat='^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$';
+
+if [[ $password =~ $pat ]]
+then
+        echo "Yes Pattern Matched Successfully !";
+else
+        echo "Please Enter a Valid Password";
+fi
